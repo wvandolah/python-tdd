2e612177924313b96b1ef755a471d6d72622b6f4
William VanDolah <william.vandolah@gmail.com>
2018-12-14 14:35:55 -0600

    added functional test to check input of a todo

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -14,14 +16,25 @@ class NewVisitorTest(unittest.TestCase):
     self.browser.get('http://localhost:8000')
     # check that titles says todo list
     self.assertIn('To-Do', self.browser.title)
-    self.fail('Finish the test!')
+    header_text = self.browser.find_element_by_tag_name('h1').header_text
+    self.assertIn('To-Do', header_text)
 
     # able to see area to input todo
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
     # able to type "fix keyboard layout"
+    inputbox.send_keys('fix keyboard layout')
     # after commiting page updates with new todo
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(any(row.text == '1: fix keyboard layout' for row in rows))
     # has the ability to add more todos
     # updates again after second todo
     # make data persiste across sessionsererer
+    self.fail("Finish the test!")
 
 if __name__ == '__main__':
   unittest.main(warnings='ignore')
